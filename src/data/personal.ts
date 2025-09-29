98d084e8c43635d9a2bdf722c02e45123ecd3daf
avicenan <avicena.n@gmail.com>
2025-09-29 11:26:29 +0700

    Add push-dir dependency and update deployment script; fix paths in personal data

--- src/data/personal.ts
+++ src/data/personal.ts
@@ -26,8 +26,8 @@ export const personalInfo: PersonalInfo = {
   bio: "I am a passionate Full Stack Developer with a love for creating innovative digital solutions.",
   longBio:
     "I am a fresh graduate in Information Systems from Telkom University with a focus on full stack web and mobile development. Experienced in academic, research, and industry projects, I enjoy creating user-centered solutions and contributing in both frontend and backend roles. As a BNSP Certified Web Developer, I am passionate about building impactful and innovative technology. I am currently open to work and collaboration opportunities in web and mobile development. I welcome roles or projects where I can apply my skills, grow professionally, and contribute to meaningful digital solutions.",
-  profileImage: "/images/profile-photo.png",
-  resumeUrl: "/documents/CV_Avicena Naufaldo_English.pdf",
+  profileImage: "./images/profile-photo.png",
+  resumeUrl: "./documents/CV_Avicena Naufaldo_English.pdf",
   githubUrl: "https://github.com/avicenan",
   linkedinUrl: "https://linkedin.com/in/avicenanaufaldo",
   instagramUrl: "https://instagram.com/avicenanaufaldo/",
